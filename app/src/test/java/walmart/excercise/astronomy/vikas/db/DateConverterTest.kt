409863610d42676daf5c2ae3825ef03ad85d8897
Vikas Patel <vikaspatel7783@gmail.com>
2022-01-11 17:07:09 +0530

    Date conversion test added DateConverterTest.kt

---
+++ app/src/test/java/walmart/excercise/astronomy/vikas/db/DateConverterTest.kt
@@ -0,0 +1,14 @@
+package walmart.excercise.astronomy.vikas.db
+
+import org.junit.Assert
+import org.junit.Test
+import walmart.excercise.astronomy.vikas.db.entity.Astronomy
+
+class DateConverterTest {
+
+    @Test
+    fun testToDateConversion() {
+        val resultDate = Astronomy.toDate(System.currentTimeMillis())
+        Assert.assertEquals("11/01/2022", resultDate)
+    }
+}
